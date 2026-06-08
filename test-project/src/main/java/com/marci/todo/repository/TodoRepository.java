00b0dc6d3095e051cb88fb1ed5ab1b82ffb0a5e1
Marcikadraga <marciprog@gmail.com>
2026-06-08 11:33:50 +0000

    add functions

--- test-project/src/main/java/com/marci/todo/repository/TodoRepository.java
+++ test-project/src/main/java/com/marci/todo/repository/TodoRepository.java
@@ -1,5 +1,14 @@
 package com.marci.todo.repository;
 
-public class TodoRepository {
-    
+import com.marci.todo.model.Todo;
+import java.util.List;
+
+public interface TodoRepository {
+    List<Todo> findAll();
+    Todo findById(Long id);
+    void save(Todo todo);
+    void update(Todo todo);
+    void deleteById(Long id);
+    List<Todo> findCompleted();
+    List<Todo> findPending();
 }
