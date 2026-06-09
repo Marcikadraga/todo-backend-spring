52572007db44b07db231f46adf627de03f5bc5ba
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-09 11:27:55 +0200

    Add nextId and priority handling in TodoService

--- test-project/src/main/java/com/marci/todo/service/TodoService.java
+++ test-project/src/main/java/com/marci/todo/service/TodoService.java
@@ -1,14 +1,17 @@
 package com.marci.todo.service;
 
+import com.marci.todo.model.Priority;
 import com.marci.todo.model.Todo;
 import com.marci.todo.repository.TodoRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class TodoService {
     private final TodoRepository todoRepository;
+    private Long nextId = 4L;
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
@@ -22,7 +25,19 @@ public class TodoService {
         return todoRepository.findById(id);
     }
 
+
+    
     public void save(Todo todo) {
+        todo.setId(nextId);
+        nextId++;
+    
+        todo.setCreatedAt(LocalDateTime.now());
+        todo.setCompleted(false);
+    
+        if (todo.getPriority() == null) {
+            todo.setPriority(Priority.NORMAL);
+        }
+    
         todoRepository.save(todo);
     }
 
