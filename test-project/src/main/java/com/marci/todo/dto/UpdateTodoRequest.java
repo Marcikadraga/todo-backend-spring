39bd4d8426568cfef8a517083c2346aeddaeac85
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-09 13:44:44 +0200

    Add fields to UpdateTodoRequest class

--- test-project/src/main/java/com/marci/todo/dto/UpdateTodoRequest.java
+++ test-project/src/main/java/com/marci/todo/dto/UpdateTodoRequest.java
@@ -1,5 +1,24 @@
 package com.marci.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import com.marci.todo.model.Priority;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UpdateTodoRequest {
-    
+    private String title;
+    private String description;
+    private boolean completed;
+    private LocalDate deadline;
+    private Priority priority;
 }
