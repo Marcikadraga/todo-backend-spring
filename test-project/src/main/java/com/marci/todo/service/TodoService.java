80520f715463c2c394e026a833dd416f7eca0400
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-09 10:38:08 +0200

    Implement CRUD operations in TodoService

--- test-project/src/main/java/com/marci/todo/service/TodoService.java
+++ test-project/src/main/java/com/marci/todo/service/TodoService.java
@@ -1,5 +1,44 @@
 package com.marci.todo.service;
 
+import com.marci.todo.model.Todo;
+import com.marci.todo.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
-    
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo getTodoById(Long id) {
+        return todoRepository.findById(id);
+    }
+
+    public void save(Todo todo) {
+        todoRepository.save(todo);
+    }
+
+    public void update(Todo todo) {
+        todoRepository.update(todo);
+    }
+
+    public void deleteById(Long id) {
+        todoRepository.deleteById(id);
+    }
+
+    public List<Todo> getCompletedTodos() {
+        return todoRepository.findCompleted();
+    }
+
+    public List<Todo> getPendingTodos() {
+        return todoRepository.findPending();
+    }
 }
