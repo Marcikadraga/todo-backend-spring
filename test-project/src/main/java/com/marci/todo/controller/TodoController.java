8a313d47752fe07d23cd59769c4cc358963101f2
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-09 10:46:40 +0200

    Implement CRUD operations for Todo items

--- test-project/src/main/java/com/marci/todo/controller/TodoController.java
+++ test-project/src/main/java/com/marci/todo/controller/TodoController.java
@@ -1,5 +1,44 @@
 package com.marci.todo.controller;
 
+import com.marci.todo.model.Todo;
+import com.marci.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoController {
-    
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Todo getTodoById(@PathVariable Long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping
+    public void createTodo(@RequestBody Todo todo) {
+        todoService.save(todo);
+    }
+
+    @PutMapping("/{id}")
+    public void updateTodo(@PathVariable Long id, @RequestBody Todo todo) {
+        todo.setId(id);
+        todoService.update(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteById(id);
+    }
 }
