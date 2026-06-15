8e93acbe99ac35edfa3cb5420c3e77102374a663
Márton Balogh <37423520+Marcikadraga@users.noreply.github.com>
2026-06-15 11:56:25 +0000

    add app user request dto

---
+++ test-project/src/main/java/com/marci/todo/dto/CreateAppUserRequest.java
@@ -0,0 +1,35 @@
+package com.marci.todo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateAppUserRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 30, message = "Username must be between 3 and 30 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    @Size(max = 120, message = "Email cannot be longer than 120 characters")
+    private String email;
+
+    @NotNull(message = "Birth date is required")
+    @Past(message = "Birth date must be in the past")
+    private LocalDate birthDate;
+}
